c525b4b9c7c51c2d2bedc868f7e603bc41e574a0
James McCarthy <jmccar16@oldwestbury.edu>
2018-02-28 14:27:07 -0500

    More models added, only about 9 more to complete

--- registration_system/models.py
+++ registration_system/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+import datetime
+YEAR_CHOICES = []
+for r in range(1980, (datetime.datetime.now().year+1)):
+    YEAR_CHOICES.append((r, r))
 
 
 # Create your models here.
 class User(models.Model):
     # TODO: Hashing strategy for password
     # TODO: encryption algorithm or nah ? password =
+    user_id = models.AutoField(primary_key=True)
     email = models.CharField(max_length=254, unique=True)
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=70)
@@ -19,33 +24,169 @@ class User(models.Model):
 
 
 class Admin(models.Model):
-    email = models.ForeignKey(User, to_field="email", db_column="email")
-    password = models.ForeignKey(User, to_field="password", db_column="password")
-    first_name = models.ForeignKey(User, to_field="first_name", db_column="first_name")
-    last_name = models.ForeignKey(User, to_field="last_name", db_column="last_name")
+    admin_id = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
 
 
 class Student(models.Model):
-    email = models.ForeignKey(User, to_field="email", db_column="email")
-    password = models.ForeignKey(User, to_field="password", db_column="password")
-    first_name = models.ForeignKey(User, to_field="first_name", db_column="first_name")
-    last_name = models.ForeignKey(User, to_field="last_name", db_column="last_name")
+    student_id = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     date_of_birth = models.DateField()
     student_type = models.CharField(max_length=1)
 
 
+class FullTimeStudent(models.Model):
+    student_id = models.OneToOneField(
+        Student,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    credits = models.IntegerField()
+
+
+class PartTimeStudent(models.Model):
+    student_id = models.OneToOneField(
+        Student,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    credits = models.IntegerField()
+
+
 class Faculty(models.Model):
     # TODO: add Department FK Constraint when Department Model is created
-    email = models.ForeignKey(User, to_field="email", db_column="email")
-    password = models.ForeignKey(User, to_field="password", db_column="password")
-    first_name = models.ForeignKey(User, to_field="first_name", db_column="first_name")
-    last_name = models.ForeignKey(User, to_field="last_name", db_column="last_name")
+    faculty_id = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     # TODO: add FK Deparment ID
     faculty_type = models.CharField(max_length=1)
 
 
+class FullTimeFaculty(models.Model):
+    faculty_id = models.OneToOneField(
+        Faculty,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+
+class PartTimeFaculty(models.Model):
+    faculty_id = models.OneToOneField(
+        Faculty,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+
 class Researcher(models.Model):
-    email = models.ForeignKey(User, to_field="email", db_column="email")
-    password = models.ForeignKey(User, to_field="password", db_column="password")
-    first_name = models.ForeignKey(User, to_field="first_name", db_column="first_name")
-    last_name = models.ForeignKey(User, to_field="last_name", db_column="last_name")
+    researcher_id = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+
+class Advising(models.Model):
+    advising_id = models.AutoField(primary_key=True)
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('faculty_id', 'student_id')
+
+
+class Hold(models.Model):
+    hold_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class StudentHold(models.Model):
+    student_hold_id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    hold_id = models.ForeignKey(Hold, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('student_id', 'hold_id')
+
+
+class Major(models.Model):
+    major_id = models.AutoField(primary_key=True)
+    # TODO: add FK Deparment ID
+    name = models.CharField(max_length=254)
+
+
+class StudentMajor(models.Model):
+    student_major_id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    major_id = models.ForeignKey(Major, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('student_id', 'major_id')
+
+
+class Minor(models.Model):
+    minor_id = models.AutoField(primary_key=True)
+    # TODO: add FK Deparment ID
+    name = models.CharField(max_length=254)
+
+
+class StudentMinor(models.Model):
+    student_minor_id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    minor_id = models.ForeignKey(Minor, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('student_id', 'minor_id')
+
+# TODO: Student History, Department, Meetings, Enrollment, Section, Course,
+# TODO: Major/Minor Requirement, Prerequisite
+# TODO: add __str__ methods
+
+
+class Semester(models.Model):
+    semester_id = models.AutoField(primary_key=True)
+    season = models.CharField(max_length=50)
+    year = models.IntegerField(_('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+
+
+class Building(models.Model):
+    building_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
+    type = models.CharField(max_length=35)
+    capacity = models.IntegerField()
+    room_number = models.CharField(max_length=5)
+
+
+class MeetingDays(models.Model):
+    days_id = models.AutoField(primary_key=True)
+    day_1 = models.CharField(max_length=10)
+    day_2 = models.CharField(max_length=10)
+
+
+class Period(models.Model):
+    period_id = models.AutoField(primary_key=True)
+    start_time = models.TimeField(input_formats="%H:%i")
+    end_time = models.TimeField(input_formats="%H:%i")
+
+
+class TimeSlot(models.Model):
+    time_slot_id = models.AutoField(primary_key=True)
+    days_id = models.ForeignKey(MeetingDays, on_delete=models.CASCADE)
+    period_id = models.ForeignKey(Period, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('days_id', 'period_id')
