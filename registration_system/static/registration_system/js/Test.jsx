cbc006743304ca78dc351a95f523b9edf2ff1020
James McCarthy <jmccar16@oldwestbury.edu>
2018-03-08 19:07:09 -0500

    Added Index Page and Some Test Data for development urposes

--- registration_system/static/registration_system/js/Test.jsx
+++ registration_system/static/registration_system/js/Test.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-class Text extends React.Component {
+class Index extends React.Component {
     render() {
         return <div>
+                <h3 className="ui center aligned header"> First Header</h3>
                 <p>This is a {this.props.test} using react-django serve-rendering</p>
+                <p>Who's brave enough to fly into something we all keep calling a death sphere? Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! Soothe us with sweet lies.</p>
+                <p>When I was first asked to make a film about my nephew, Hubert Farnsworth, I thought "Why should I?" Then later, Leela made the film. But if I did make it, you can bet there would have been more topless women on motorcycles. Roll film! Hi, I'm a naughty nurse, and I really need someone to talk to. $9.95 a minute.</p>
+                <p>Yes! In your face, Gandhi! Leela, are you alright? You got wanged on the head. You guys go on without me! I'm going to go… look for more stuff to steal! Yeah, and if you were the pope they'd be all, "Straighten your pope hat." And "Put on your good vestments."</p>
+                <p>Hey, tell me something. You've got all this money. How come you always dress like you're doing your laundry? Tell her she looks thin. WINDMILLS DO NOT WORK THAT WAY! GOOD NIGHT! Say what? Good man. Nixon's pro-war and pro-family.</p>
+                <p>WINDMILLS DO NOT WORK THAT WAY! GOOD NIGHT! And yet you haven't said what I told you to say! How can any of us trust you? Stop! Don't shoot fire stick in space canoe! Cause explosive decompression! We don't have a brig.</p>
+                <p>Enough about your promiscuous mother, Hermes! We have bigger problems. I was all of history's great robot actors - Acting Unit 0.8; Thespomat; David Duchovny! Um, is this the boring, peaceful kind of taking to the streets?</p>
+                <p>A true inspiration for the children. Hey, guess what you're accessories to. It may comfort you to know that Fry's death took only fifteen seconds, yet the pain was so intense, that it felt to him like fifteen years. And it goes without saying, it caused him to empty his bowels.</p>
+                <p>Or a guy who burns down a bar for the insurance money! I'm a thing. Belligerent and numerous. No, just a regular mistake.</p>
+                <p>OK, if everyone's finished being stupid. Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! No argument here. You are the last hope of the universe.</p>
 
-        </div>
+            </div>
     }
 }
 
-export default Text;
+export default Index;
